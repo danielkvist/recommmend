3618a9863e0e234492e9b0daf5db972923f0bf94
Daniel <danielkvist@protonmail.com>
2019-12-19 18:03:37 +0100

    add csrf helper

--- actions/render.go
+++ actions/render.go
@@ -1,6 +1,8 @@
 package actions
 
 import (
+	"html/template"
+
 	"github.com/gobuffalo/buffalo/render"
 	"github.com/gobuffalo/packr/v2"
 )
@@ -10,19 +12,14 @@ var assetsBox = packr.New("app:assets", "../public")
 
 func init() {
 	r = render.New(render.Options{
-		// HTML layout to be used for all HTML requests:
-		HTMLLayout: "application.plush.html",
-
-		// Box containing all of the templates:
+		HTMLLayout:   "application.plush.html",
 		TemplatesBox: packr.New("app:templates", "../templates"),
 		AssetsBox:    assetsBox,
 
-		// Add template helpers here:
 		Helpers: render.Helpers{
-			// for non-bootstrap form helpers uncomment the lines
-			// below and import "github.com/gobuffalo/helpers/forms"
-			// forms.FormKey:     forms.Form,
-			// forms.FormForKey:  forms.FormFor,
+			"csrf": func() template.HTML {
+				return template.HTML("<input name=\"authenticity_token\" value=\"<%= authenticity_token %>\" type=\"hidden\">")
+			},
 		},
 	})
 }
