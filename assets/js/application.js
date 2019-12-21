8a11085ff693f3305635257b5c28974d3739db20
Daniel <danielkvist@protonmail.com>
2019-12-21 14:07:12 +0100

    add recommended template and update templates

--- assets/js/application.js
+++ assets/js/application.js
@@ -2,3 +2,6 @@ require("expose-loader?$!expose-loader?jQuery!jquery");
 require("bootstrap/dist/js/bootstrap.bundle.js");
 
 $(() => {});
+$("body").click(function(){  
+    $(".alert").alert("close");
+});
